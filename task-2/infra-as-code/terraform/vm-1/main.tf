122c0264ab5e7af851736ff0eb2b59de74240a94
ryantasat57 <ryanta.nugraha@agriaku.com>
2025-06-05 19:45:49 +0700

    add task-2

--- task-2/infra-as-code/terraform/vm-1/main.tf
+++ task-2/infra-as-code/terraform/vm-1/main.tf
@@ -11,17 +11,17 @@ module "default" {
     source                          = "../module/ec2-instance"
     aws_region                      = "ap-southeast-1"
     availability_zones               = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
-    name                            = "ubuntu"
+    name                            = "vm-1"
     environment                     = "development"
     project                         = "infra"
     key_name                        = "ryanta-profile"
     iam_instance_profile            = "ServerRole"
-    security_groups                 = ["sg-030198b3e099e2620","sg-01106abb20f500c1c"]
+    security_groups                 = ["sg-xxxxx","sg-0xxxxx"]
     ami                             = "ami-0750a20e9959e44ff"
     instance_type                   = "t3.micro"
     count_start                     = "1"
     count_instance                  = "1"
-    subnets                         = ["subnet-06a114a7ef004e36e", "subnet-03af21ec655b8267e", "subnet-0e73e42d9b6626ff5"]  
+    subnets                         = ["subnet-0xxxx", "subnet-0xxxx", "subnet-00xxxx"]  
     root_disk                       = "20"
     root_disk_type                  = "gp3"
 
